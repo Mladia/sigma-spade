a16cb6346a4d653ff9e002bd907b005c821687f3
Nikolay Mladenov <nidenov@gmail.com>
2023-09-23 02:12:54 +0200

    adding auto init functionality

--- docker/run.sh
+++ docker/run.sh
@@ -13,12 +13,16 @@ fi
 if ! [ -f "$spade_executable_path" ]
 then
     echo "$spade_executable_path does not exist."
-    cd /tmp/
-    git clone https://github.com/ashish-gehani/SPADE.git
-    cp -r /tmp/SPADE/* /SPADE/
+    echo Performing initial SPADE configuration...
+    
+    cd /SPADE && git clone https://github.com/ashish-gehani/SPADE.git
 
-    ./configure
-    ./make -j 8
+    echo Starting the build
+    cd /SPADE && ./configure
+    echo Building SPADE
+    make -C /SPADE
+    sudo chown root /SPADE/bin/spadeAuditBridge
+    sudo chmod ug+s /SPADE/bin/spadeAuditBridge
 fi
 
 $spade_executable_path stop
